f68c4bc94ab45877195cf2d39b30441698ff9eff
tjagadishqualminds <tjagadish@qualminds.com>
2024-05-08 16:11:37 +0530

    Exceptions

---
+++ Assignment2/Assignment2/Excepitons/ExceptionHandling.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Excepitons
+{
+    internal class ExceptionHandling
+    {
+        public void exception()
+        {
+
+            //Exceptions
+
+            try
+            {
+                Console.WriteLine("User details");
+                Console.WriteLine("Enter user id");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter name");
+                string name = Console.ReadLine();
+                Console.WriteLine(name.ToUpper());
+              if(string.IsNullOrEmpty(name))
+                {
+                    throw new NullReferenceException();
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message + "id should be numeric and should not be empty string");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+      
+
+
+        }
+
+    }
+
+
+
+
+}
+
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Assignment2.Excepitons;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Exceptions
+          ExceptionHandling exception = new ExceptionHandling();
+            exception.exception();
+
+            Console.ReadLine();
+        }
+    }
+}
