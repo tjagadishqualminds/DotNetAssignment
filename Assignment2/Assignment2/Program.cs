c8c844e1e1d4df6beb22872fa64820a71b300161
tjagadishqualminds <tjagadish@qualminds.com>
2024-05-14 15:25:29 +0530

    sql three task  completed

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -25,55 +25,55 @@ namespace Assignment2
             exceptionone.exceptionone();
 
             ////Exceptionstwo
-            ExceptionHandling exception = new ExceptionHandling();
-            exception.exception();
+            //ExceptionHandling exception = new ExceptionHandling();
+            //exception.exception();
 
             //extensionsmethods
-            ExtensionMethod method = new ExtensionMethod();
-            method.Test1();
-            method.Test2();
-            method.Test3();
+            //ExtensionMethod method = new ExtensionMethod();
+            //method.Test1();
+            //method.Test2();
+            //method.Test3();
 
-            int x = 5;
-            long result = x.Factorial();
+            //int x = 5;
+            //long result = x.Factorial();
 
-            Console.WriteLine(result);
-            string str = "hello world";
-            Console.WriteLine(str.ToProper());
+            //Console.WriteLine(result);
+            //string str = "hello world";
+            //Console.WriteLine(str.ToProper());
 
-            operations operations = new operations();
-            while (true)
-            {
+            //operations operations = new operations();
+            //while (true)
+            //{
 
-                Console.WriteLine("Enter your options \n 0. AddElement \n 1.PrintElement \n 2.DeleteElement,\n 3.DisplaySingleElement,\n 4.CreateList\n 5. Listtodictionary");
-                int choice = int.Parse(Console.ReadLine());
-                Enum @enum = (options)choice;
+            //    Console.WriteLine("Enter your options \n 0. AddElement \n 1.PrintElement \n 2.DeleteElement,\n 3.DisplaySingleElement,\n 4.CreateList\n 5. Listtodictionary");
+            //    int choice = int.Parse(Console.ReadLine());
+            //    Enum @enum = (options)choice;
 
-                switch (@enum)
-                {
-                    case options.AddElement:
-                        operations.addEle();
-                        break;
-                    case options.PrintElement:
-                        operations.printELe();
-                        break;
-                    case options.DeleteElement:
-                        operations.deleteEle();
-                        break;
-                    case options.DisplaySingleElement:
-                        operations.singleEle();
-                        break;
-                    case options.CreateList:
-                        operations.listofkeys();
-                        break;
-                    case options.Listtodictionary:
-                        operations.listToDic();
-                        break;
+            //    switch (@enum)
+            //    {
+            //        case options.AddElement:
+            //            operations.addEle();
+            //            break;
+            //        case options.PrintElement:
+            //            operations.printELe();
+            //            break;
+            //        case options.DeleteElement:
+            //            operations.deleteEle();
+            //            break;
+            //        case options.DisplaySingleElement:
+            //            operations.singleEle();
+            //            break;
+            //        case options.CreateList:
+            //            operations.listofkeys();
+            //            break;
+            //        case options.Listtodictionary:
+            //            operations.listToDic();
+            //            break;
 
 
-                }
+            //    }
 
-            }
+            //}
 
 
 
