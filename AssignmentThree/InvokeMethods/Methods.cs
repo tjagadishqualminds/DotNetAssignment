6460d6d7930c8cebbab61ac39e2c962197ff289e
tjagadishqualminds <tjagadish@qualminds.com>
2024-05-15 16:17:01 +0530

    invoke methodone

---
+++ AssignmentThree/InvokeMethods/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentThree.InvokeMethods
+{
+    public class Methods
+    {
+    //1. value type by value.
+        public void methodOne(int x)
+        {
+            x = x * 2;
+         Console.WriteLine(x);
+        }
+    //2.value type by reference.
+
+        public int methodTwo( ref int x)
+        {
+            x = x + 2;
+             return x;
+        }
+    //3.reference type by value.
+
+    //4.reference type by reference.
+
+        public string Name { get;set; }
+
+
+
+    }
+}
---
+++ AssignmentThree/Program.cs
@@ -0,0 +1,48 @@
+﻿using AssignmentThree.Abstract;
+using AssignmentThree.InvokeMethods;
+using AssignmentThree.InvokeMethodTwo;
+using AssignmentThree.Permutations;
+
+namespace AssignmentThree
+{
+    internal class Program
+    {
+        static void ModifyPersonName(Methods methods)
+        {
+            methods.Name = "jagadish";
+        }
+        static void ModifyPersonNametwo( ref Methods methods)
+        {
+            methods.Name = "jagadish";
+        }
+        static void Main(string[] args)
+        {
+            int num = 12;
+            int num2 = 12;
+           //one
+           Methods methods = new Methods();
+            methods.methodOne(num);
+
+            int result = methods.methodTwo(ref num2);
+            Console.WriteLine(result);
+
+         
+            Methods method = new Methods { Name = "ALDKSF"};
+             ModifyPersonName(method);
+            Console.WriteLine(method.Name);
+
+            Methods method3 = new Methods { Name = "ALDKSF" };
+            ModifyPersonNametwo( ref method3);
+            Console.WriteLine(method.Name);
+
+
+  
+
+
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
