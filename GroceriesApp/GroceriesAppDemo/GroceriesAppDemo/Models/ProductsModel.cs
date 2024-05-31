63b59d5a36a24be3afee48be03c75ba2a59ddad4
tjagadishqualminds <tjagadish@qualminds.com>
2024-05-31 14:52:07 +0530

    get api created

---
+++ GroceriesApp/GroceriesAppDemo/GroceriesAppDemo/Models/ProductsModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroceriesAppDemo.Models
+{
+    public class ProductsModel
+    {
+        [Key]
+        public int ProductsId { get; set; }
+
+        public int ProductSupplierId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+        public int ProductCategoryId { get; set; }
+    }
+}
