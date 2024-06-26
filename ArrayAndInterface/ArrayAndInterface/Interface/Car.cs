1818eb03aaa50a8250cd30ef6396a22fbbe7cd05
tjagadishqualminds <tjagadish@qualminds.com>
2024-06-26 14:25:38 +0530

    Add Interface

---
+++ ArrayAndInterface/ArrayAndInterface/Interface/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndInterface.Interface
+{
+    public class Car : IVehicle
+    {
+        int gasoline;
+        public Car(int gasoLine) { 
+            gasoline = gasoLine;
+        }
+       public  void Drive()
+        {
+          if(gasoline > 0)
+            {
+                Console.WriteLine("car is driving ");
+            }
+          else
+            {
+                Console.WriteLine("refuel the car");
+            }
+        }
+
+       public bool Refuel(int amount)
+        {
+            gasoline += amount;
+            return true;
+        }
+    }
+}
